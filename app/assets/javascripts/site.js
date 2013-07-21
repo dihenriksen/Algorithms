470f717b1304c7f1bfc672f50dabf36cdf939521
Daniel Henriksen <danhenriksen12@gmail.com>
2013-07-20 19:20:34 -0700

    Display total swaps for bubblesort

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -3,7 +3,7 @@
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
 function bubblesort(array) {
-	var swaps = 0;
+	var swaps = 0;	// counts total number of swaps within the array
 	var sorted = false;
 	var new_array_element = $('p').first().clone().html('');
 	var array_for_display = [];
@@ -15,23 +15,34 @@ function bubblesort(array) {
 				swap(array, i, i+1);
 				swaps++;
 			}
-
-			// array_for_display = new_array_element.html(array);
-			// console.log(array_for_display);
-			// $('.bubblesort').append(array_for_display);
-
-			new_array_element = $('p').first().clone().html(array);
-			$('.bubblesort').append(new_array_element);
 		}
 		if (for_testing_if_any_swaps === swaps) {
 			sorted = true;
 		}
+
+	// array_for_display = new_array_element.html(array);
+	// console.log(array_for_display);
+	// $('.bubblesort').append(array_for_display);
+
+	new_array_element = $('p').first().clone().html(array);
+	$('.bubblesort').append(new_array_element);
+
 	}
+
+	$('.bubblesort').append('<p>' + swaps + ' swaps</p>');
 	return array;
 }
 
+function mergesort(array) {
+	if (array.length === 1) {
+		return array;
+	} else {
+
+	}
+}
+
 function swap(array, x, y) {
-	// This function swaps the positions of two elements in an array
+	// Swaps the positions of two elements in an array
 	var temp = array[x];
 	array[x] = array[y];
 	array[y] = temp;
